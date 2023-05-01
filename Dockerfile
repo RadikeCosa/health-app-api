cde5694a8eb22d79d42ff90ba1d1266d82e387e2
Ramiro Cosa <ramirocosa@gmail.com>
2023-04-30 23:27:07 -0300

    probando

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk:11
+MAINTAINER Radike
+
+COPY  target/healthAPI-0.0.1-SNAPSHOT.jar healthAPI-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","/healthAPI-0.0.1-SNAPSHOT.jar"]
+
---
+++ src/main/java/com/chinatown/healthAPI/WebConfig.java
@@ -0,0 +1,15 @@
+package com.chinatown.healthAPI;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry.addMapping("/**");
+    }
+}
